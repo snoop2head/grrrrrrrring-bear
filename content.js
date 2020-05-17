aed8f9a2e2cbe2905bed1279c44da255343761e8
noopy <young_ahn@yonsei.ac.kr>
2020-05-17 19:13:34 +0900

    add bear word counter to extension

--- content.js
+++ content.js
@@ -1,1 +1,9 @@
-alert("grrrrrrr");
+// alert("grrrrrrr");
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  // regular expression rule
+  const re = new RegExp("bear", "gi");
+  // yield matches corresponding to the regular expression rule
+  const matches = document.documentElement.innerHTML.match(re);
+  sendResponse({ count: matches.length });
+});
